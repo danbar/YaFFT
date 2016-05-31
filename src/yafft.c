09097cdb4a5dacd294bd860da42753358cccd41c
Daniel Bartel <danbar@users.noreply.github.com>
2016-05-31 22:07:38 +0200

    add masking to bit reversal

--- src/yafft.c
+++ src/yafft.c
@@ -3,7 +3,6 @@
  *
  */
 
-#include <limits.h>
 #include <math.h>
 
 #include "yafft.h"
@@ -14,16 +13,17 @@
  * https://graphics.stanford.edu/~seander/bithacks.html
  *
  */
-static inline unsigned int reverse_bits(unsigned int v, const unsigned int bits) {
-	unsigned int r = v; // r will be reversed bits of v; first get LSB of v
-	int s = bits - 1; // extra shift needed at end
+static inline unsigned int reverse_bits(unsigned int v, const unsigned int num_bits) {
+	unsigned int r = v;   // r will be reversed bits of v
+	int s = num_bits - 1; // extra shift needed at end
 
 	for (v >>= 1; v; v >>= 1) {
 		r <<= 1;
 		r |= v & 1;
 		s--;
 	}
-	r <<= s; // shift when v's highest bits are zero
+	r <<= s;                  // shift when v's highest bits are zero
+	r &= (1 << num_bits) - 1; // mask out non-significant bits
 	return r;
 }
 
@@ -40,11 +40,11 @@ static inline void swap(complex float* a, complex float* b) {
  */
 void fft(complex float* data, const unsigned int n) {
 	// Initialize variables
-	const unsigned int log2n = log2(n);
+	const unsigned int num_stages = log2(n);
 
 	// Bit reversal
 	for (unsigned int i = 0; i < n; i++) {
-	    unsigned int j = reverse_bits(i, log2n);
+	    unsigned int j = reverse_bits(i, num_stages);
 	    if (i < j) {
 	        swap(&data[i], &data[j]);
 	    }
