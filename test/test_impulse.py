e3be1244055a0509552a9f0257b0eb991ba6be7e
Daniel Bartel <danbar@users.noreply.github.com>
2016-06-01 21:48:42 +0200

    add C implementation to test_impulse

--- test/test_impulse.py
+++ test/test_impulse.py
@@ -19,34 +19,35 @@ class Test2PointFFT(unittest.TestCase):
         octave.restart()
         octave.addpath('./octave')
         octave.addpath('./test/octave')
-        octave.eval('pkg load signal')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        self.in1 = np.array([1, 0], dtype=np.complex64)
+        self.out1 = np.array([1, 1], dtype=np.complex64)
+
+        self.in2 = np.array([0, 1], dtype=np.complex64)
+        self.out2 = np.array([1, -1], dtype=np.complex64)
 
     def tearDown(self):
         octave.exit()
 
-    def test_2point_fft_using_octave_implementation(self):
-        test_in = np.array([[1, 0]], dtype=np.complex64)
-        test_out = np.array([[1, 1]], dtype=np.complex64)
+    def test_octave_2point_fft(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
-
-        test_in = np.array([[0, 1]], dtype=np.complex64)
-        test_out = np.array([[1, -1]], dtype=np.complex64)
-
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in2)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out2)
 
     def test_2point_fft(self):
-        data = np.array([1, 0], dtype=np.complex64)
-        #
+        data = self.in1
         yafft.fft(data)
-        print(data)
-        #
-        #data = yafft.new_data(1)
-        #yafft.fft(data, 1)
-        #print(yafft.data_getitem(data, 0))
-        #yafft.delete_data(data)
+        npt.assert_almost_equal(data, self.out1)
+
+        data = self.in2
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out2)
 
 
 class Test4PointFFT(unittest.TestCase):
@@ -55,35 +56,57 @@ class Test4PointFFT(unittest.TestCase):
         octave.restart()
         octave.addpath('./octave')
         octave.addpath('./test/octave')
-        octave.eval('pkg load signal')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        self.in1 = np.array([1, 0, 0, 0], dtype=np.complex64)
+        self.out1 = np.array([1, 1, 1, 1], dtype=np.complex64)
+
+        self.in2 = np.array([0, 1, 0, 0], dtype=np.complex64)
+        self.out2 = np.array([1, -1j, -1, 1j], dtype=np.complex64)
+
+        self.in3 = np.array([0, 0, 1, 0], dtype=np.complex64)
+        self.out3 = np.array([1, -1, 1, -1], dtype=np.complex64)
+
+        self.in4 = np.array([0, 0, 0, 1], dtype=np.complex64)
+        self.out4 = np.array([1, 1j, -1, -1j], dtype=np.complex64)
 
     def tearDown(self):
         octave.exit()
 
-    def test_4point_fft_using_octave_implementation(self):
-        test_in = np.array([[1, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, 1, 1, 1]], dtype=np.complex64)
+    def test_octave_4point_fft(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in2)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out2)
 
-        test_in = np.array([[0, 1, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -1j, -1, 1j]], dtype=np.complex64)
+        res = octave.my_fft(self.in3)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out3)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in4)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out4)
 
-        test_in = np.array([[0, 0, 1, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -1, 1, -1]], dtype=np.complex64)
+    def test_4point_fft(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in2
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out2)
 
-        test_in = np.array([[0, 0, 0, 1]], dtype=np.complex64)
-        test_out = np.array([[1, 1j, -1, -1j]], dtype=np.complex64)
+        data = self.in3
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out3)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in4
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out4)
 
 
 class Test8PointFFT(unittest.TestCase):
@@ -92,61 +115,103 @@ class Test8PointFFT(unittest.TestCase):
         octave.restart()
         octave.addpath('./octave')
         octave.addpath('./test/octave')
-        octave.eval('pkg load signal')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        v = 1/np.sqrt(2)
+
+        self.in1 = np.array([1, 0, 0, 0, 0, 0, 0, 0], dtype=np.complex64)
+        self.out1 = np.array([1, 1, 1, 1, 1, 1, 1, 1], dtype=np.complex64)
+
+        self.in2 = np.array([0, 1, 0, 0, 0, 0, 0, 0], dtype=np.complex64)
+        self.out2 = np.array([1, v-1j*v, -1j, -v-1j*v, -1, -v+1j*v, 1j, v+1j*v], dtype=np.complex64)
+
+        self.in3 = np.array([0, 0, 1, 0, 0, 0, 0, 0], dtype=np.complex64)
+        self.out3 = np.array([1, -1j, -1, 1j, 1, -1j, -1, 1j], dtype=np.complex64)
+
+        self.in4 = np.array([0, 0, 0, 1, 0, 0, 0, 0], dtype=np.complex64)
+        self.out4 = np.array([1, -v-1j*v, 1j, v-1j*v, -1, v+1j*v, -1j, -v+1j*v], dtype=np.complex64)
+
+        self.in5 = np.array([0, 0, 0, 0, 1, 0, 0, 0], dtype=np.complex64)
+        self.out5 = np.array([1, -1, 1, -1, 1, -1, 1, -1], dtype=np.complex64)
+
+        self.in6 = np.array([0, 0, 0, 0, 0, 1, 0, 0], dtype=np.complex64)
+        self.out6 = np.array([1, -v+1j*v, -1j, v+1j*v, -1, v-1j*v, 1j, -v-1j*v], dtype=np.complex64)
+
+        self.in7 = np.array([0, 0, 0, 0, 0, 0, 1, 0], dtype=np.complex64)
+        self.out7 = np.array([1, 1j, -1, -1j, 1, 1j, -1, -1j], dtype=np.complex64)
+
+        self.in8 = np.array([0, 0, 0, 0, 0, 0, 0, 1], dtype=np.complex64)
+        self.out8 = np.array([1, v+1j*v, 1j, -v+1j*v, -1, -v-1j*v, -1j, v-1j*v], dtype=np.complex64)
 
     def tearDown(self):
         octave.exit()
 
-    def test_8point_fft_using_octave_implementation(self):
-        v = 1/np.sqrt(2)
+    def test_octave_8point_fft(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1)
 
-        test_in = np.array([[1, 0, 0, 0, 0, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.complex64)
+        res = octave.my_fft(self.in2)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out2)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in3)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out3)
 
-        test_in = np.array([[0, 1, 0, 0, 0, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, v-1j*v, -1j, -v-1j*v, -1, -v+1j*v, 1j, v+1j*v]], dtype=np.complex64)
+        res = octave.my_fft(self.in4)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out4)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in5)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out5)
 
-        test_in = np.array([[0, 0, 1, 0, 0, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -1j, -1, 1j, 1, -1j, -1, 1j]], dtype=np.complex64)
+        res = octave.my_fft(self.in6)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out6)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        res = octave.my_fft(self.in7)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out7)
 
-        test_in = np.array([[0, 0, 0, 1, 0, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -v-1j*v, 1j, v-1j*v, -1, v+1j*v, -1j, -v+1j*v]], dtype=np.complex64)
+        res = octave.my_fft(self.in8)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out8)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+    def test_8point_fft(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1)
 
-        test_in = np.array([[0, 0, 0, 0, 1, 0, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -1, 1, -1, 1, -1, 1, -1]], dtype=np.complex64)
+        data = self.in2
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out2)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in3
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out3)
 
-        test_in = np.array([[0, 0, 0, 0, 0, 1, 0, 0]], dtype=np.complex64)
-        test_out = np.array([[1, -v+1j*v, -1j, v+1j*v, -1, v-1j*v, 1j, -v-1j*v]], dtype=np.complex64)
+        data = self.in4
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out4)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in5
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out5)
 
-        test_in = np.array([[0, 0, 0, 0, 0, 0, 1, 0]], dtype=np.complex64)
-        test_out = np.array([[1, 1j, -1, -1j, 1, 1j, -1, -1j]], dtype=np.complex64)
+        data = self.in6
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out6)
 
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in7
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out7)
 
-        test_in = np.array([[0, 0, 0, 0, 0, 0, 0, 1]], dtype=np.complex64)
-        test_out = np.array([[1, v+1j*v, 1j, -v+1j*v, -1, -v-1j*v, -1j, v-1j*v]], dtype=np.complex64)
-
-        test_res = octave.my_fft(test_in)
-        npt.assert_almost_equal(test_res, test_out)
+        data = self.in8
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out8)
 
 
 if __name__ == '__main__':
