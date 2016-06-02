d4b055c021057a3f0cce66df412c084d2d544d4f
Daniel Bartel <danbar@users.noreply.github.com>
2016-06-02 21:21:27 +0200

    add C implementation to test_signal

--- test/test_signal.py
+++ test/test_signal.py
@@ -13,33 +13,123 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'build', 'swig'))
 import yafft
 
 
-class TestSine(unittest.TestCase):
+def _create_sine(num_samples):
+    fs = 1000  # Hz, sampling frequency
+
+    n = np.arange(0., num_samples)
+    T = 1./fs  # s, sampling period
+    t = n*T  # s, time vector
+
+    x = np.sin(2.*np.pi*50.*t)
+    return x.astype(np.complex64)
+
+
+class Test16SamplesSine(unittest.TestCase):
 
     def setUp(self):
         octave.restart()
         octave.addpath('./octave')
         octave.addpath('./test/octave')
-        octave.eval('pkg load signal')
+        octave.eval('pkg load signal')  # load signal package
 
-        # Parameters
-        N = 1000  # length of signal
-        fs = 1000  # Hz, sampling frequency
-
-        # Signals
-        n = np.arange(0., N-1.)
-        T = 1./fs  # s, sampling period
-        t = n*T  # s, time vector
-
-        x = np.sin(2.*np.pi*50.*t)
-        self.x = x.astype(np.complex128)[None, :]
+        # Test data
+        N = 16
+        self.in1 = _create_sine(N)
+        self.out1 = np.squeeze(octave.fft(self.in1, N))
 
     def tearDown(self):
         octave.exit()
 
-    def test_sine_using_octave_implementation(self):
-        X = octave.my_fft(self.x)
-        Y = np.fft.fft(self.x, 1024)
-        npt.assert_almost_equal(X, Y, decimal=6)
+    def test_octave_sine(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1, decimal=6)
+
+    def test_sine(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1, decimal=6)
+
+
+class Test32SamplesSine(unittest.TestCase):
+
+    def setUp(self):
+        octave.restart()
+        octave.addpath('./octave')
+        octave.addpath('./test/octave')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        N = 32
+        self.in1 = _create_sine(N)
+        self.out1 = np.squeeze(octave.fft(self.in1, N))
+
+    def tearDown(self):
+        octave.exit()
+
+    def test_octave_sine(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1, decimal=6)
+
+    def test_sine(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1, decimal=6)
+
+
+class Test64SamplesSine(unittest.TestCase):
+
+    def setUp(self):
+        octave.restart()
+        octave.addpath('./octave')
+        octave.addpath('./test/octave')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        N = 64
+        self.in1 = _create_sine(N)
+        self.out1 = np.squeeze(octave.fft(self.in1, N))
+
+    def tearDown(self):
+        octave.exit()
+
+    def test_octave_sine(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1, decimal=5)
+
+    def test_sine(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1, decimal=5)
+
+
+class Test128SamplesSine(unittest.TestCase):
+
+    def setUp(self):
+        octave.restart()
+        octave.addpath('./octave')
+        octave.addpath('./test/octave')
+        octave.eval('pkg load signal')  # load signal package
+
+        # Test data
+        N = 128
+        self.in1 = _create_sine(N)
+        self.out1 = np.squeeze(octave.fft(self.in1, N))
+
+    def tearDown(self):
+        octave.exit()
+
+    def test_octave_sine(self):
+        res = octave.my_fft(self.in1)
+        res = np.squeeze(res)
+        npt.assert_almost_equal(res, self.out1, decimal=5)
+
+    def test_sine(self):
+        data = self.in1
+        yafft.fft(data)
+        npt.assert_almost_equal(data, self.out1, decimal=5)
 
 
 if __name__ == '__main__':
